e291dbd28a10658b451a0e9d8fdb6e8e5ccbff73
Hugo Hirsch <github@hugo-hirsch.de>
2021-06-03 13:22:51 +0200

    Init project - still working

---
+++ src/main/java/com/example/demo/ValidationCompileErrorApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@SpringBootApplication
+public class ValidationCompileErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationCompileErrorApplication.class, args);
+	}
+
+}
+
+class ValidationInputForm {
+    @Size(min = 1, max = 10)
+    @NotEmpty(message = "{NotEmpty.option.options}")
+    private List<@NotEmpty(message = "{NotEmpty.option.options}") String> options = new ArrayList<>();
+}
